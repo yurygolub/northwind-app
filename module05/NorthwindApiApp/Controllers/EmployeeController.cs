d5233b0731cf78c60cc3dd1181d8dd9172097b8d
Yury Golub <yuragolub21@gmail.com>
2022-03-28 13:31:56 +0300

    Add existing projects.

---
+++ module05/NorthwindApiApp/Controllers/EmployeeController.cs
@@ -0,0 +1,104 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Services.Employees;
+
+#pragma warning disable SA1600
+
+namespace NorthwindApiApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeManagementService managementService;
+        private readonly IEmployeePicturesService picturesService;
+
+        public EmployeeController(IEmployeeManagementService managementService, IEmployeePicturesService picturesService)
+        {
+            this.managementService = managementService;
+            this.picturesService = picturesService;
+        }
+
+        [HttpGet]
+        public IActionResult GetEmployees([FromQuery] int offset = 0, [FromQuery] int limit = 10)
+        {
+            return this.Ok(this.managementService.GetEmployeesAsync(offset, limit));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployeeAsync(int id)
+        {
+            var employee = await this.managementService.GetEmployeeAsync(id);
+            if (employee is null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(employee);
+        }
+
+        [HttpGet("{id}/photo")]
+        public async Task<IActionResult> GetEmployeePhotoAsync(int id)
+        {
+            var employee = await this.managementService.GetEmployeeAsync(id);
+            if (employee is null)
+            {
+                return this.NotFound();
+            }
+
+            return this.File(employee.Photo[78..], "image/bmp");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployeeAsync([FromBody] Employee employee)
+        {
+            await this.managementService.CreateEmployeeAsync(employee);
+            return this.Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployeeAsync(int id)
+        {
+            if (!await this.managementService.DestroyEmployeeAsync(id))
+            {
+                return this.NotFound();
+            }
+
+            return this.NoContent();
+        }
+
+        [HttpDelete("{id}/photo")]
+        public async Task<IActionResult> DeleteEmployeePhotoAsync(int id)
+        {
+            if (!await this.picturesService.DeleteEmployeePictureAsync(id))
+            {
+                return this.NotFound();
+            }
+
+            return this.NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployeeAsync(int id, [FromBody] Employee employee)
+        {
+            if (!await this.managementService.UpdateEmployeeAsync(id, employee))
+            {
+                return this.NotFound();
+            }
+
+            return this.NoContent();
+        }
+
+        [HttpPut("{id}/photo")]
+        public async Task<IActionResult> UpdateProductCategoryPictureAsync(int id, [FromBody] Stream stream)
+        {
+            if (!await this.picturesService.UpdateEmployeePictureAsync(id, stream))
+            {
+                return this.NotFound();
+            }
+
+            return this.NoContent();
+        }
+    }
+}
---
+++ module05/NorthwindApiApp/ServiceCollectionExtension.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Northwind.Services;
+using Northwind.Services.Employees;
+using Northwind.Services.Products;
+using Northwind.Services.SqlServer;
+using DataAccess = Northwind.Services.DataAccess;
+
+#pragma warning disable SA1600
+
+namespace NorthwindApiApp
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddSqlServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services
+                .AddTransient<IProductManagementService, DataAccess.Products.ProductManagementService>()
+                .AddTransient<IProductCategoryManagementService, DataAccess.Products.ProductCategoryManagementService>()
+                .AddTransient<IProductCategoryPicturesService, DataAccess.Products.ProductCategoryPicturesService>()
+                .AddTransient<IEmployeeManagementService, DataAccess.Employees.EmployeeManagementService>()
+                .AddTransient<IEmployeePicturesService, DataAccess.Employees.EmployeePicturesService>()
+                .AddScoped(s => new SqlConnection(configuration.GetConnectionString("SqlConnection")))
+                .AddTransient<NorthwindDataAccessFactory, SqlServerDataAccessFactory>();
+        }
+    }
+}
