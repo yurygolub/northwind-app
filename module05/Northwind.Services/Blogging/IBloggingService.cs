61bbcc75b5bc8d7d3f13e744b67132908c3a7648
Yury Golub <yuragolub21@gmail.com>
2022-04-08 13:00:18 +0300

    Add implementation.

--- module05/Northwind.Services/Blogging/IBloggingService.cs
+++ module05/Northwind.Services/Blogging/IBloggingService.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Northwind.Services.Blogging
 {
     public interface IBloggingService
     {
+        IAsyncEnumerable<BlogArticle> GetBlogArticlesAsync(int offset, int limit);
+
+        Task<BlogArticle> GetBlogArticleAsync(int blogArticleId);
+
+        Task<int> CreateBlogArticleAsync(BlogArticle blogArticle);
+
+        Task<bool> DeleteBlogArticleAsync(int blogArticleId);
+
+        Task<bool> UpdateBlogArticleAsync(int blogArticleId, BlogArticle blogArticle);
     }
 }
